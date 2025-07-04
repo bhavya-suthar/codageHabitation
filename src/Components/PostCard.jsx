bc3e2cbbe334e9d2d5d94eeac174fc4235c6b967
Bhavya Suthar <bhavyasuthar06@gmail.com>
2025-07-04 21:47:54 +0530

    task completed

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -10,11 +10,11 @@ const PostCard = ({ item }) => {
   }
   return (
     <>
-      <div className="card w-25 m-3" onClick={handleClick}>
+      <div className="card w-25 h-25 m-1" onClick={handleClick}>
         <p className="card-header"><span className="font-weight-bold">Title: </span>{item.title}</p>
-        <p className="card-body"><span className="font-weight-bold">Description: </span>{item.body}</p>
-        <p className="card-body"><span className="font-weight-bold">Tags: </span>#{item.tags}</p>
-        <p className="text-primary card-body" style={{cursor:"pointer"}} onClick={handleClick}><span className="font-weight-bold">Author Name: </span>{item.author.name}</p>
+        <p className="card-body p-2"><span className="font-weight-bold">Description: </span>{item.body}</p>
+        <p className="card-body p-1"><span className="font-weight-bold">Tags: </span>#{item.tags.join(" #")}</p>
+        <p className="text-primary card-body p-1" style={{cursor:"pointer"}} onClick={handleClick}><span className="font-weight-bold">Author Name: </span>{item.author.name}</p>
       </div>
     </>
   );
